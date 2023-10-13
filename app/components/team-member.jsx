0db55aae82f8633c076ddbf538c3aa2b617fc739
hartmandy <120344775+hartmandy@users.noreply.github.com>
2023-10-13 18:29:02 -0400

    edited our team section

--- app/components/team-member.jsx
+++ app/components/team-member.jsx
@@ -8,7 +8,7 @@ export default function TeamMember({ member }) {
       <img
         src={member.img}
         alt={member.name}
-        className="w-full h-64 object-cover mb-3"
+        className="w-full h-84 object-cover mb-3"
       />
       <h2 className="font-bold text-2xl mb-2">{member.name}</h2>
       <p className="font-medium text-lg mb-2">{member.title}</p>
